55f1e835dbc25dc7608e62ad417df2eb56fe0bb5
Tim Joosten <Tjoosten3@gmail.com>
2016-11-26 21:37:16 +0100

    Fix #14 | Add copyright headers to classes

--- application/controllers/Signature.php
+++ application/controllers/Signature.php
@@ -1,5 +1,13 @@
 <?php defined('BASEPATH') OR exit('No direct script access allowed');
 
+/**
+ * @package       Begrotingstekort-petitie
+ * @subpackage    Controllers
+ * @category      Activism
+ * @version       1.0.0
+ * @author        Tim Joosten
+ * @link          https://github.com/Activisme-be/Begrotingstekort-Petitie
+ */
 class Signature extends CI_Controller
 {
     public $Session = [];
--- application/controllers/Welcome.php
+++ application/controllers/Welcome.php
@@ -1,7 +1,12 @@
 <?php defined('BASEPATH') OR exit('No direct script access allowed');
 
 /**
- * Class Welcome
+ * @package       Begrotingstekort-petitie
+ * @subpackage    Controllers
+ * @category      Activism
+ * @version       1.0.0
+ * @author        Tim Joosten
+ * @link          https://github.com/Activisme-be/Begrotingstekort-Petitie
  */
 class Welcome extends CI_Controller {
 
