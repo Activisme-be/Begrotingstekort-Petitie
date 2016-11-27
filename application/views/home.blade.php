467bb5ffcdce87701457bbf0969824838cf33796
Tim Joosten <Tjoosten3@gmail.com>
2016-11-27 11:32:23 +0100

    Add signature insert form

--- application/views/home.blade.php
+++ application/views/home.blade.php
@@ -15,6 +15,52 @@
                             Met deze speciale petitie proberen wij duidelijk te maken dat er andere manieren zijn. <br>
                             Wij burgers doen het zelf, voel u vrij om de regering een handje toe te steken! <br>
                         </p>
+
+                        <hr>
+
+                        <form method="POST" action="" class="form-horizontal">
+                            <div class="form-group">
+                                <label class="control-label col-sm-2">
+                                    Uw naam: <span class="text-danger">*</span>
+                                </label>
+                                <div class="col-sm-5">
+                                    <input type="text" name="name" placeholder="Uw naam" class="form-control" />
+                                </div>
+                            </div>
+
+                            <div class="form-group">
+                                <label class="col-sm-2 control-label">
+                                    Email: <span class="text-danger">*</span>
+                                </label>
+                                <div class="col-sm-5">
+                                    <input type="text" class="form-control" name="email" placeholder="Uw email adres" />
+                                </div>
+                            </div>
+
+                            <div class="form-group">
+                                <label class="col-sm-2 control-label">
+                                    Vonst: <span class="text-danger">*</span>
+                                </label>
+
+                                <div class="col-sm-5">
+                                    <select class="form-control" name="finding">
+                                        <option value="" selected>-- Selecteer uw vonst --</option>
+                                        <option value="1000">(1.000€) Lux leaks</option>
+                                        <option value="10000">(10.000€) Swiss Leaks</option>
+                                        <option value="100000">(100.000€) Panama Papers</option>
+                                        <option value="1000000">(1.000.000€) Bahama leaks</option>
+                                        <option value="10000000">(10.000.000€) Vermogens taks</option>
+                                    </select>
+                                </div>
+                            </div>
+
+                            <div class="form-group">
+                                <div class="col-sm-offset-2 col-sm-4">
+                                    <button type="submit" class="btn btn-sm btn-success">Insturen</button>
+                                    <button type="reset" class="btn btn-sm btn-danger">Reset</button>
+                                </div>
+                            </div>
+                        </form>
                     </div>
 
                     <div class="col-md-3">
